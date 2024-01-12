ace3101aa8c94533bccb5b83ff0801e18938e4de
Esen Tekkanat <esentekkanat@gmail.com>
2024-01-12 16:03:03 +0300

    Google auth basics implemented

--- App/Pages/Login.js
+++ App/Pages/Login.js
@@ -2,10 +2,18 @@ import { View, Text ,Image, StyleSheet, Button, TouchableOpacity } from 'react-n
 import React,{useState, useEffect} from 'react'
 import Colors from '../Shared/Colors'
 import { Ionicons } from '@expo/vector-icons';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
 
 
 
 export default function Login() {
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId: '55959786226-e9frfu2d60hu3lt653blch82e4rhjsnp.apps.googleusercontent.com',
+    expoClientId:'55959786226-llk648p590tvtaoklnv4o89mtjtenecr.apps.googleusercontent.com'
+   
+  });
 
   return (
     <View>
@@ -16,7 +24,7 @@ export default function Login() {
   
         <Text style={{textAlign:'center',
         marginTop:100, fontSize:20}} >Login/Signup</Text>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={()=>promptAsync()}>
       <Ionicons name="logo-google" size={24} 
       color="white" style={{marginRight:10}} />
         <Text style ={{color:Colors.white}}>Sign In with Google</Text>
